03c7a404ae18fc864d20621674c65b33b1d5e99f
Roshan Thapa <roshanrays236@gmail.com>
2025-09-22 18:30:58 +0545

    Calculator v1.0

--- calc.js
+++ calc.js
@@ -1,5 +1,7 @@
 
 let buffer = '0';
+let runningTotal = 0;
+let previousOperator = null;
 
 const screen = document.querySelector('.calc-display');
 
@@ -16,13 +18,71 @@ function handleNumber(number) {
     if (buffer === '0') {
         buffer = number;
     } else {
+
         buffer += number;
     }
-    console.log(buffer)
+    
+}
+
+function handleMath(value) {
+    if (buffer === 0) {
+        return; //do nothing
+    }
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+    previousOperator = value;
+    buffer = '0';
+}
+
+function flushOperation (intBuffer) {
+    switch (previousOperator) {
+        case '+':
+            runningTotal += intBuffer;
+            break;
+        case '-':
+            runningTotal -= intBuffer;
+            break;
+        case '/':
+            runningTotal /= intBuffer;
+            break;
+        case '*':
+            runningTotal *= intBuffer;
+            break;
+    }
 }
 
 function handleSymbol(symbol) {
-    console.log('symbol')
+    switch(symbol) {
+        case 'C':
+            buffer = '0';
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return; // no math needed
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length == 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length-1)
+            }
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            handleMath(symbol);
+            break;
+    }
 }
 
 function init() {
