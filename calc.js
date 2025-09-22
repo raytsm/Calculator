20a83ab3708bb2ece367054040eba7be04a6904d
Roshan Thapa <roshanrays236@gmail.com>
2025-09-22 11:53:22 +0545

    WIP

--- calc.js
+++ calc.js
@@ -1,5 +1,40 @@
-const calc = document.querySelector(".calculator")
-const display = document.querySelector(".calc-display")
-calc.addEventListener("click", function(event) {
-    display.innerText = event.target.innerText;
-})
+
+let buffer = '0';
+
+const screen = document.querySelector('.calc-display');
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    rerender()
+}
+
+function handleNumber(number) {
+    if (buffer === '0') {
+        buffer = number;
+    } else {
+        buffer += number;
+    }
+    console.log(buffer)
+}
+
+function handleSymbol(symbol) {
+    console.log('symbol')
+}
+
+function init() {
+    document
+        .querySelector(".calculator")
+        .addEventListener("click", function(event) {
+            buttonClick(event.target.innerText);
+        })
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
+
+init()
